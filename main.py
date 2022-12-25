f19ece8867866d8e7f6692a014e9021af6f763cb
YUHEI <80562226+aspiratio@users.noreply.github.com>
2022-12-25 16:43:08 +0900

    jsonをcsvに変換するための最低限の処理を実装

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+# JSONファイルを読み込んで辞書型に変換
+with open("test.json", "r") as f:
+    json_dict = json.load(f)
+
+# CSVファイルに書き込み
+with open("output.csv", "w", newline="") as f:
+    writer = csv.DictWriter(
+        f,  # 読み込み対象のファイル
+        fieldnames=json_dict[0].keys(),  # ヘッダー
+        doublequote=True,  # 文字列内にquotechar（")が含まれるとき、同じquotechar（")でエスケープするか
+        quoting=csv.QUOTE_ALL,  # 全てのフィールドをquotecharで囲む
+    )
+    writer.writeheader()  # ヘッダーに fieldnames に指定したkeyのリストを出力
+    writer.writerows(json_dict)  # 辞書を渡してvalueを書き込む
